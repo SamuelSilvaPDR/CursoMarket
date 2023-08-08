f502aa4d4112e4d6aaace2e62ad043089f513f71
Samuel Silva <samuel.oliveira1@estudante.ifgoiano.edu.br>
2023-08-08 12:13:36 -0300

    Criando tabela

---
+++ src/main/resources/db/migration/V1__create-table-produto.sql
@@ -0,0 +1,10 @@
+create table produto(
+    id int not null auto_increment,
+    nome varchar(100) not null,
+    peso varchar(10) not null,
+    tipo varchar(20) not null,
+    valor float not null,
+    validade varchar(50) not null,
+    
+    primary key(id)
+)
